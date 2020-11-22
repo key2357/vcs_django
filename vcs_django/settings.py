3605a1c79f670f24219228568409fc43f840ad2f
zephyr <a550196504@gmail.com>
2020-11-22 22:51:27 +0800

    change db psw

--- vcs_django/settings.py
+++ vcs_django/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vcs',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'azaqwsx123',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
