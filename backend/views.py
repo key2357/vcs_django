94fca4e025954804f4a4f49eba05b1d0e0dd1451
YJ <1216200079@qq.com>
2020-12-02 09:29:03 +0800

    2020.12.2 9:28:00 update

--- backend/views.py
+++ backend/views.py
@@ -15,12 +15,328 @@ import networkx as nx
 
 # import matplotlib.pyplot as plt
 def test(request):
-    cursor = connection.cursor()
-    cursor.execute("select count(similarity) from similarity_info where similarity = 1")
-    desc = cursor.description
-    all_data = cursor.fetchall()
-    row_data = [dict(zip([col[0] for col in desc], row)) for row in all_data]
-    print(row_data)
+    i = 0
+    time_time_json = []
+    while i < 1:
+        b, e = get_time_str(i, i + 0.0033333)
+        time_time_json.append([b, e])
+        i += 0.0033333
+    print(len(time_time_json))
+    jsonData = json.dumps(time_time_json)
+    fileObject = open('time_300_slices.json', 'w')
+    fileObject.write(jsonData)
+    fileObject.close()
+
+    # with open(str(BASE_DIR) + '//clustering.json', 'r', encoding='utf8')as fp:
+    #     json_data = json.load(fp)
+    #
+    # data = json_data['allData']
+    # for i in range(len(data)):  # region
+    #     region = data[i]
+    #     for j in range(len(region['Region_VPC'])):  # VPC
+    #         vpc = region['Region_VPC'][j]
+    #         for k in range(len(vpc['AS_ECS'])):  # AS
+    #             az = vpc['AS_ECS'][k]
+    #             az.pop('VPC_ID')
+    #             az.pop('Region_ID')
+    #             # data[i]['Region_VPC'][j]['AS_ECS'][k] = az
+    #
+    # jsonData = json.dumps(data)
+    # fileObject = open('cluster.json', 'w')
+    # fileObject.write(jsonData)
+    # fileObject.close()
+
+    # begin_time = 0
+    # end_time = 0.01
+    # b, e = get_time_str(begin_time, end_time)
+    # print(b, e)
+    # print(2 + 8)
+    # slice = {
+    #     'beginTime': 0,
+    #     'endTime': 1
+    # }
+    #
+    # file_filter = {
+    #     'malwareType': [],
+    #     'malwareSubtype': [],
+    #     'fileType': []
+    # }
+    #
+    # file = {
+    #     'categories': 'malwareSubtype',
+    #     'subtype': 'WEBSHELL'
+    # }
+    #
+    # # change file
+    # # file = change_file(file)
+    #
+    # has_filter = has_filter_func(file_filter)
+    #
+    # # 时间片或文件过滤为空的逻辑，以确定where_str
+    # if has_filter and slice:
+    #     begin_time_number = slice['beginTime']
+    #     end_time_number = slice['endTime']
+    #     begin_time_str, end_time_str = get_time_str(begin_time_number, end_time_number)
+    #     malware_type_list = file_filter['malwareType']
+    #     malware_subtype_list = file_filter['malwareSubtype']
+    #     malware_filetype_list = file_filter['fileType']
+    #     where_str = get_file_and_time_where_str(malware_type_list, malware_subtype_list, malware_filetype_list,
+    #                                             begin_time_str, end_time_str)
+    # elif has_filter:  # 加个时间会不会比较好
+    #     malware_type_list = file_filter['malwareType']
+    #     malware_subtype_list = file_filter['malwareSubtype']
+    #     malware_filetype_list = file_filter['fileType']
+    #     begin_time_number = 0
+    #     end_time_number = 1
+    #     begin_time_str, end_time_str = get_time_str(begin_time_number, end_time_number)
+    #     where_str = get_file_and_time_where_str(malware_type_list, malware_subtype_list, malware_filetype_list,
+    #                                             begin_time_str, end_time_str)
+    # elif slice:
+    #     begin_time_number = slice['beginTime']
+    #     end_time_number = slice['endTime']
+    #     begin_time_str, end_time_str = get_time_str(begin_time_number, end_time_number)
+    #     where_str = get_time_where_str(begin_time_str, end_time_str)
+    # else:
+    #     begin_time_number = 0
+    #     where_str = ''
+    # cursor = connection.cursor()
+    #
+    # cursor.execute(
+    #     "select uuid AS ESC_ID, AS_ID, VPC_ID, Region_ID, "
+    #     "count(malware_type) AS malwareNumber, "
+    #     "sum(case when malware_type='WEBSHELL'then 1 else 0 end) as WEBSHELL, "
+    #     "sum(case when malware_type='DDOS木马' then 1 else 0 end) as DDOS木马,"
+    #     "sum(case when malware_type='被污染的基础软件' then 1 else 0 end) as 被污染的基础软件,"
+    #     "sum(case when malware_type='恶意程序' then 1 else 0 end) as 恶意程序,"
+    #     "sum(case when malware_type='恶意脚本文件' then 1 else 0 end) as 恶意脚本文件,"
+    #     "sum(case when malware_type='感染型病毒' then 1 else 0 end) as 感染型病毒,"
+    #     "sum(case when malware_type='黑客工具' then 1 else 0 end) as 黑客工具,"
+    #     "sum(case when malware_type='后门程序' then 1 else 0 end) as 后门程序,"
+    #     "sum(case when malware_type='勒索病毒' then 1 else 0 end) as 勒索病毒,"
+    #     "sum(case when malware_type='漏洞利用程序' then 1 else 0 end) as 漏洞利用程序,"
+    #     "sum(case when malware_type='木马程序' then 1 else 0 end) as 木马程序,"
+    #     "sum(case when malware_type='蠕虫病毒' then 1 else 0 end) as 蠕虫病毒,"
+    #     "sum(case when malware_type='挖矿程序' then 1 else 0 end) as 挖矿程序,"
+    #     "sum(case when malware_type='自变异木马' then 1 else 0 end) as 自变异木马, "
+    #     "sum(case when malware_class='网站后门' then 1 else 0 end) as 网站后门, "
+    #     "sum(case when malware_class='恶意进程' then 1 else 0 end) as 恶意进程, "
+    #     "sum(case when malware_class='恶意脚本' then 1 else 0 end) as 恶意脚本 "
+    #     "from malware_base_info AS a LEFT JOIN user_netstate_info AS b ON a.uuid=b.ECS_ID " + where_str + " group by uuid")
+    #
+    # desc = cursor.description
+    # all_data = cursor.fetchall()
+    # ecs_force_and_file = [dict(zip([col[0] for col in desc], row)) for row in all_data]
+    #
+    # if not ecs_force_and_file:
+    #     ReturnData = {}
+    #     return HttpResponse(json.dumps(ReturnData), content_type='application/json')
+    #
+    # # webshell, DDOS木马,被污染的基础软件,恶意程序,恶意脚本文件,感染型病毒,黑客工具,后门程序,勒索病毒,漏洞利用程序,木马程序,蠕虫病毒,挖矿程序,自变异木马
+    # # 处理文件信息
+    # file_info_count = 0
+    # file_info = []
+    # file_info_number = []
+    # for d in ecs_force_and_file:
+    #     file_info.append({})
+    #     file_info_number.append({})
+    #     all_file_number = d['malwareNumber']
+    #     for sub_type in MALWARE_SUBTYPE:
+    #         if int(d[sub_type]) != 0:
+    #             file_info_number[file_info_count][sub_type] = int(d[sub_type])
+    #             file_info[file_info_count][sub_type] = int(d[sub_type]) / all_file_number
+    #     file_info_count += 1
+    #
+    # # 再修改一些文件信息的格式
+    # file_info_result = []
+    #
+    # for i in range(len(file_info)):
+    #     file_info_result.append([])
+    #     for f_key in file_info[i]:
+    #         file_info_result[i].append(
+    #             {
+    #                 'filename': f_key,
+    #                 'percent': file_info[i][f_key],
+    #                 'fileNum': file_info_number[i][f_key]
+    #             }
+    #         )
+    #
+    # # 态势值对应的 danger | warn | safe | none 全改为文件数量
+    # # 先求得态势值10% 60%的值
+    # file_num = []  # 保存态势值的数值
+    # total_value = 0
+    # for d in ecs_force_and_file:
+    #     file_num.append(int(d['malwareNumber']))
+    #     total_value += int(d['malwareNumber'])
+    # level_value_sort = file_num
+    # level_value_sort = sorted(level_value_sort)
+    # point90 = level_value_sort[math.floor(len(level_value_sort) / 10 * 9)]
+    # point40 = level_value_sort[math.floor(len(level_value_sort) / 10 * 4)]
+    # point97 = level_value_sort[math.floor(len(level_value_sort) / 10 * 9.7)]
+    # level_value_info = []  # 保存态势值的颜色
+    # is_extremely_dangerous = []  # 保存ecs是否高危
+    # for le in file_num:
+    #     if le == 0:
+    #         level_value_info.append('none')
+    #     elif le < point40:
+    #         level_value_info.append('safe')
+    #     elif point40 <= le < point90:
+    #         level_value_info.append('warn')
+    #     else:
+    #         level_value_info.append('danger')
+    #     if le > point97:
+    #         is_extremely_dangerous.append(True)
+    #     else:
+    #         is_extremely_dangerous.append(False)
+    #
+    # # 处理一下阈值 9 和 64  小于9不显示圆形 大于64显示file_info
+    # radius = []
+    # for d in ecs_force_and_file:
+    #     malware_number = d['malwareNumber']
+    #     if malware_number < 9:
+    #         radius.append(0)
+    #     elif 9 <= malware_number <= 64:
+    #         radius.append((math.sqrt(malware_number) - 3) / 5 * 10)
+    #     else:
+    #         radius.append(-1)
+    #
+    # # 计算是否高亮
+    # is_highlight = []
+    # if file['categories'] != '' and file['subtype'] != '':
+    #     categories = file['categories']
+    #     subtype = file['subtype']
+    #     for d in ecs_force_and_file:
+    #         if categories == 'FileType':
+    #             if subtype == 'Webshell':
+    #                 subtype = 'WEBSHELL'
+    #             elif subtype == '二进制':
+    #                 subtype = '恶意进程'
+    #             else:
+    #                 subtype = '恶意脚本'
+    #             if d[subtype] > 0:
+    #                 is_highlight.append(True)
+    #             else:
+    #                 is_highlight.append(False)
+    #         else:
+    #             if d[subtype] > 0:
+    #                 is_highlight.append(True)
+    #             else:
+    #                 is_highlight.append(False)
+    # else:
+    #     for i in range(len(ecs_force_and_file)):
+    #         is_highlight.append(False)
+    #
+    # df = pd.read_csv(str(BASE_DIR) + '//backend//data//malware_cluster.csv', usecols=[1, 21])
+    # data_color = df.iloc[:, 0:2].values
+    # data_color_dict = {}
+    # for i in range(len(data_color)):
+    #     data_color_dict[data_color[i][0]] = data_color[i][1]
+    #
+    # # 从小到大嵌套
+    # AS_ECS_TYPE = []
+    # for i in range(len(ecs_force_and_file)):
+    #     AS_ECS_TYPE.append({
+    #         'ECS_ID': ecs_force_and_file[i]['ESC_ID'],
+    #         # 'type': level_value_info[i],
+    #         # 'radius': radius[i],
+    #         # 'fileInfo': file_info_result[i],
+    #         # # 'forceValue': math.sqrt(level_value[i]),
+    #         # 'isExtremelyDangerous': is_extremely_dangerous[i],
+    #         # 'isHighLight': is_highlight[i],
+    #         'color': data_color_dict[ecs_force_and_file[i]['ESC_ID']]
+    #     })
+    #
+    # AS_ECS = []
+    # AS_ECS_set = set()
+    # for i in range(len(AS_ECS_TYPE)):
+    #     # 判断是否含有AS
+    #     if ecs_force_and_file[i]['AS_ID'] not in AS_ECS_set:
+    #         AS_ECS.append({
+    #             # 'ECS_NUM': 1,
+    #             'Region_ID': ecs_force_and_file[i]['Region_ID'],
+    #             'VPC_ID': ecs_force_and_file[i]['VPC_ID'],
+    #             'AS_ID': ecs_force_and_file[i]['AS_ID'],
+    #             'AS_ECS_TYPE': [AS_ECS_TYPE[i]]
+    #         })
+    #         AS_ECS_set.add(ecs_force_and_file[i]['AS_ID'])
+    #     else:
+    #         for AS in AS_ECS:
+    #             if AS['AS_ID'] == ecs_force_and_file[i]['AS_ID']:
+    #                 AS['AS_ECS_TYPE'].append(AS_ECS_TYPE[i])
+    #                 # AS['ECS_NUM'] += 1
+    #
+    # Region_VPC = []
+    # Region_VPC_set = set()
+    #
+    # for i in range(len(AS_ECS)):
+    #     # 判断是否含有vpc
+    #     if AS_ECS[i]['VPC_ID'] not in Region_VPC_set:
+    #         Region_VPC.append({
+    #             # 'AS_NUM': 1,
+    #             # 'ECS_NUM': AS_ECS[i]['ECS_NUM'],
+    #             'VPC_ID': AS_ECS[i]['VPC_ID'],
+    #             'AS_ECS': [AS_ECS[i]]
+    #         })
+    #         Region_VPC_set.add(AS_ECS[i]['VPC_ID'])
+    #     else:
+    #         for VPC in Region_VPC:
+    #             if VPC['VPC_ID'] == AS_ECS[i]['VPC_ID']:
+    #                 VPC['AS_ECS'].append(AS_ECS[i])
+    #                 # VPC['AS_NUM'] += 1
+    #                 # VPC['ECS_NUM'] += AS_ECS[i]['ECS_NUM']
+    # allData = []
+    # Region_set = set()
+    # for i in range(len(Region_VPC)):
+    #     # 判断是否含有vpc
+    #     if Region_VPC[i]['AS_ECS'][0]['Region_ID'] not in Region_set:
+    #         allData.append({
+    #             # 'VPC_NUM': 1,
+    #             # 'AS_NUM': Region_VPC[i]['AS_NUM'],
+    #             # 'ECS_NUM': Region_VPC[i]['ECS_NUM'],
+    #             'Region_ID': Region_VPC[i]['AS_ECS'][0]['Region_ID'],
+    #             'Region_VPC': [Region_VPC[i]]
+    #         })
+    #         Region_set.add(Region_VPC[i]['AS_ECS'][0]['Region_ID'])
+    #     else:
+    #         for Region in allData:
+    #             if Region['Region_ID'] == Region_VPC[i]['AS_ECS'][0]['Region_ID']:
+    #                 Region['Region_VPC'].append(Region_VPC[i])
+    #                 # Region['VPC_NUM'] += 1
+    #                 # Region['AS_NUM'] += Region_VPC[i]['AS_NUM']
+    #                 # Region['ECS_NUM'] += Region_VPC[i]['ECS_NUM']
+    #
+    # # 查询文件数量
+    # cursor.execute("select count(*) AS malwareNumber, "
+    #                "SUM(CASE file_type WHEN 'WEBSHELL' Then 1 ELSE 0 END) AS webshellNumber, "
+    #                "SUM(CASE file_type WHEN 'BIN' Then 1 ELSE 0 END) AS biNumber, "
+    #                "SUM(CASE file_type WHEN 'SCRIPT' Then 1 ELSE 0 END) AS scriptNumber from "
+    #                "(select file_type from malware_base_info " + where_str + "group by malware_md5) t")
+    #
+    # desc = cursor.description
+    # all_data = cursor.fetchall()
+    # file_data = [dict(zip([col[0] for col in desc], row)) for row in all_data]
+    # ReturnData = {
+    #     # 'timeStamp': begin_time_number,
+    #     # 'totalValue': total_value,
+    #     'allData': allData,
+    #     # 'files': {
+    #     #     'malwareNumber': file_data[0]['malwareNumber'],
+    #     #     'webshellNumber': int(str(file_data[0]['webshellNumber'])),
+    #     #     'biNumber': int(str(file_data[0]['biNumber'])),
+    #     #     'scriptNumber': int(str(file_data[0]['scriptNumber'])),
+    #     # }
+    # }
+    #
+    # jsonData = json.dumps(ReturnData)
+    # fileObject = open('clustering.json', 'w')
+    # fileObject.write(jsonData)
+    # fileObject.close()
+
+    # cursor = connection.cursor()
+    # cursor.execute("select count(similarity) from similarity_info where similarity = 1")
+    # desc = cursor.description
+    # all_data = cursor.fetchall()
+    # row_data = [dict(zip([col[0] for col in desc], row)) for row in all_data]
+    # print(row_data)
     # 处理全连接图为环形
     # error_node = [123, 143, 149, 166, 187, 250, 284]  # 包含全连接和非全连接的
     # error_node_all_connection = [105, 242]
@@ -281,9 +597,47 @@ def get_ecs_force(request):
     all_data = cursor.fetchall()
     ecs_force_and_file = [dict(zip([col[0] for col in desc], row)) for row in all_data]
 
-    if not ecs_force_and_file:
-        ReturnData = {}
-        return HttpResponse(json.dumps(ReturnData), content_type='application/json')
+    # 再取出所有的ECS
+    cursor.execute("select ECS_ID AS ESC_ID, AS_ID, VPC_ID, Region_ID from user_netstate_info")
+    desc = cursor.description
+    all_data = cursor.fetchall()
+    all_ecs = [dict(zip([col[0] for col in desc], row)) for row in all_data]
+
+    # 先判断ecs_force_and_file中的ecs
+    ecs_set = set()
+    for e in ecs_force_and_file:
+        ecs_set.add(e['ESC_ID'])
+
+    for a in all_ecs:
+        if a['ESC_ID'] not in ecs_set:
+            ecs_force_and_file.append({
+                'ESC_ID': a['ESC_ID'],
+                'AS_ID': a['AS_ID'],
+                'VPC_ID': a['VPC_ID'],
+                'Region_ID': a['Region_ID'],
+                'malwareNumber': 0,
+                'WEBSHELL': 0,
+                'DDOS木马': 0,
+                '被污染的基础软件': 0,
+                '恶意程序': 0,
+                '恶意脚本文件': 0,
+                '感染型病毒': 0,
+                '黑客工具': 0,
+                '后门程序': 0,
+                '勒索病毒': 0,
+                '漏洞利用程序': 0,
+                '木马程序': 0,
+                "蠕虫病毒": 0,
+                "挖矿程序": 0,
+                "自变异木马": 0,
+                "网站后门": 0,
+                "恶意进程": 0,
+                "恶意脚本": 0,
+            })
+
+    # if not ecs_force_and_file:
+    #     ReturnData = {}
+    #     return HttpResponse(json.dumps(ReturnData), content_type='application/json')
 
     # webshell, DDOS木马,被污染的基础软件,恶意程序,恶意脚本文件,感染型病毒,黑客工具,后门程序,勒索病毒,漏洞利用程序,木马程序,蠕虫病毒,挖矿程序,自变异木马
     # 处理文件信息
@@ -477,20 +831,20 @@ def get_ecs_force(request):
 
 # 用真数据
 def get_force_graph_by_time(request):
-    params = json.loads(request.body)
-    slice = params['slice']
-    file_filter = params['fileFilter']
+    # params = json.loads(request.body)
+    # slice = params['slice']
+    # file_filter = params['fileFilter']
 
-    # slice = {
-    #     'beginTime': 0,
-    #     'endTime': 1
-    # }
-    #
-    # file_filter = {
-    #     'malwareType': [],
-    #     'malwareSubtype': [],
-    #     'fileType': []
-    # }
+    slice = {
+        'beginTime': 0,
+        'endTime': 1
+    }
+
+    file_filter = {
+        'malwareType': [],
+        'malwareSubtype': [],
+        'fileType': []
+    }
 
     # 时间片为空的逻辑，以确定where_str
     if slice:
