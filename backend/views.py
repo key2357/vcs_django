007f244757c85b0623d9ffee9f6e7689d725df46
YJ <1216200079@qq.com>
2020-12-07 13:04:09 +0800

    2020.12.7 13:04:00 update

--- backend/views.py
+++ backend/views.py
@@ -150,31 +150,31 @@ def get_force(request):
 
 # view3 态势等级视图 获取每个ECS的信息（包括ECS_ID、态势等级、聚类结果、半径、态势值、是否高危、是否高亮、文件信息）
 def get_ecs_force(request):
-    # params = json.loads(request.body)
-    # slice = params['slice']
-    # file_filter = params['fileFilter']
-    # file = params['file']
+    params = json.loads(request.body)
+    slice = params['slice']
+    file_filter = params['fileFilter']
+    file = params['file']
 
-    alpha = 0.5
-    beta = 0.5
-    theta = 0.5
-    gamma = 0.5
-
-    slice = {
-        'beginTime': 0.58,
-        'endTime': 0.6
-    }
-
-    file_filter = {
-        'malwareType': ['网站后门', '恶意进程'],
-        'malwareSubtype': ['WEBSHELL', '挖矿程序'],
-        'fileType': ['BIN', 'WEBSHELL']
-    }
-
-    file = {
-        'categories': 'malwareSubtype',
-        'subtype': 'WEBSHELL'
-    }
+    # alpha = 0.5
+    # beta = 0.5
+    # theta = 0.5
+    # gamma = 0.5
+    #
+    # slice = {
+    #     'beginTime': 0.58,
+    #     'endTime': 0.6
+    # }
+    #
+    # file_filter = {
+    #     'malwareType': ['网站后门', '恶意进程'],
+    #     'malwareSubtype': ['WEBSHELL', '挖矿程序'],
+    #     'fileType': ['BIN', 'WEBSHELL']
+    # }
+    #
+    # file = {
+    #     'categories': 'malwareSubtype',
+    #     'subtype': 'WEBSHELL'
+    # }
 
     # change file
     # file = change_file(file)
